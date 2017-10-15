f1a888f34f2f227c78cd95e1796085a47055a75d
Ruslan Synytsky <siruslan@gmail.com>
2017-10-15 22:52:10 +0200

    added several retries to connect to gitlab

--- scripts/tokens.js
+++ scripts/tokens.js
@@ -1,19 +1,24 @@
 //@req(pswd, domain, next)
 import com.hivext.api.core.utils.Transport;
 
+//domain = "${env.domain}";
 /** 
  * creating Gitlab impersonation token
  */
+
 //using root user
-var login = "root", userId = 1;
+var login = "root",
+    userId = 1;
 
 var t = new Transport();
-var resp = t.post("https://" + domain + "/api/v4/session/", {
+
+var resp = post("https://" + domain + "/api/v4/session/", {
     login: login,
     password: pswd
-}, {});
-
+}, {})
+if (resp.result == 99) return resp;
 resp = toNative(resp);
+
 var privateToken = resp.private_token;
 if (!privateToken) return {
     result: 99,
@@ -21,14 +26,15 @@ if (!privateToken) return {
     respone: resp
 };
 
-var resp = t.post("https://" + domain + "/api/v4/users/" + userId + "/impersonation_tokens", {
+var resp = post("https://" + domain + "/api/v4/users/" + userId + "/impersonation_tokens", {
     "name": "token-for-jelastic",
     "scopes[]": "api"
 }, {
     "PRIVATE-TOKEN": privateToken
 });
-
+if (resp.result == 99) return resp;
 resp = toNative(resp);
+
 var impToken = resp.token;
 if (!impToken) return {
     result: 99,
@@ -54,7 +60,26 @@ resp = {
     onAfterReturn: {}
 };
 resp.onAfterReturn[next] = {
-    GITLAB_IMP_TOKEN: impToken,
+    GIT_IMP_TOKEN: impToken,
     J_TOKEN: jToken
 };
 return resp;
+
+function post(url, params, headers) {
+    var i = 0,
+        sleep = 10000,
+        ntimes = 12;
+
+    //trying [ntimes] with [sleep] ms delay between two requests 
+    while (i++ < ntimes) {
+        try {
+            return t.post(url, params, headers);
+        } catch (e) {
+            if (i == ntimes) return {
+                result: 99,
+                error: e
+            };
+            else java.lang.Thread.sleep(sleep);
+        }
+    }
+}
